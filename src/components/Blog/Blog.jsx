589a4809ef89fb1d63748ccacda60b4e8e9388d1
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-09-06 17:35:50 +0600

    blog page done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,48 +1,77 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { BlogContext } from "../../context/BlogContextComponent";
 import BlogContent from "../BlogContent/BlogContent";
+import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
+
 
 
 const Blog = () => {
-    const {blogProducts,loading} = useContext(BlogContext)
-    if(loading){
-        return <h1 className="text-red-700 text-center">Loading...</h1>
-    }
+  const { blogProducts, loading, } = useContext(BlogContext);
+  const [dataLength, setDataLength] = useState(6);
+  if (loading) {
+    return <h1 className="text-red-700 text-center">Loading...</h1>;
+  }
 
-    
-    return (
-        <div className="container mx-auto">
-            <hr className="mt-5"/>
-            <h2 className="text-center text-3xl font-bold py-5"> Hasan Furniture {blogProducts.length}</h2>
+  return (
+    <div className="container mx-auto">
+      <hr className="mt-5" />
+      <h2 className="text-center text-3xl font-bold py-5">
+        {" "}
+        Hasan Furniture {blogProducts.length}
+      </h2>
 
-            <div className="grid grid-cols-3 md:grid-cols-8 lg:grid-cols-12 gap-5">
+      <div className="grid grid-cols-3 md:grid-cols-8 lg:grid-cols-12 gap-5 pb-12">
+        {/* sidebar */}
+        <div className="col-span-1 md:col-span-2 lg:col-span-2 border-r-2 pr-4">
+          <div className="font-bold space-y-4">
+            <button className="px-5 py-2.5 hover:bg-black bg-base-300 rounded-lg w-full hover:text-white text-black ">
+              Rocking chair
+            </button>
 
-                   {/* sidebar */}
-                <div className="col-span-1 md:col-span-2 lg:col-span-2 border-r-2 pr-4">
-                    <div className="font-bold space-y-4">
-                          <button className="px-5 py-2.5 hover:bg-black bg-base-300 rounded-lg w-full hover:text-white text-black ">Rocking chair</button>
+            <button className="px-5 py-2.5 hover:bg-black bg-base-300 rounded-lg w-full hover:text-white text-black ">
+              Side chair
+            </button>
 
-                          <button className="px-5 py-2.5 hover:bg-black bg-base-300 rounded-lg w-full hover:text-white text-black ">Side chair</button>
-
-                          <button className="px-5 py-2.5 hover:bg-black bg-base-300 rounded-lg w-full hover:text-white text-black ">Lounge chair</button>
-                    </div>
-                 
-
-                </div>
-                <div className="col-span-2 md:col-span-6 lg:col-span-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                    {
-                        blogProducts.map(product => <BlogContent key={product.id} product={product}></BlogContent>)
-                           
-                    }
-                   
-                   
-
-                </div>
-
-            </div>
-            
+            <button className="px-5 py-2.5 hover:bg-black bg-base-300 rounded-lg w-full hover:text-white text-black ">
+              Lounge chair
+            </button>
+          </div>
         </div>
-    );
+        <div className="col-span-2 md:col-span-6 lg:col-span-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {blogProducts.slice(0, dataLength).map((product) => (
+            <BlogContent key={product.id} product={product}></BlogContent>
+          ))}
+        </div>
+      </div>
+
+      <div
+        className={`text-center pl-44 pb-5 ${dataLength === 15 && "hidden"}`}
+      >
+        <button
+          onClick={() => setDataLength(blogProducts.length)}
+          className="btn btn-warning  text-xl"
+        >
+          Show All
+        </button>
+      </div>
+
+      <div className="flex justify-center pl-44 pb-5">
+       
+            <div className="join ">
+            
+                <button className="join-item btn">  <FaChevronLeft></FaChevronLeft></button>
+                <button className="join-item btn btn-active">1</button>
+                <button className="join-item btn ">2</button>
+                <button className="join-item btn">3</button>
+                <button className="join-item btn">4</button>
+                <button className="join-item btn"><FaChevronRight></FaChevronRight></button>
+            </div>
+      </div>
+
+   
+    </div>
+  );
 };
 
 export default Blog;
--- src/components/BlogContent/BlogContent.jsx
+++ src/components/BlogContent/BlogContent.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types';
 
 const BlogContent = ({product}) => {
     // const key = Object.keys(product).join(',');
@@ -10,16 +10,23 @@ const BlogContent = ({product}) => {
                 <div className="bg-[#F2F2F2] rounded-lg p-10">
                     <img src={product_img} alt="" className="w-full h-48 object-cover" />
                 </div>
-                <h2 className="font-bold text-2xl py-4 px-1">{product_title}</h2>
-                <div>
-                    <span className="text-sm text-gray-500"> ${discount_price}</span>
-                    <span className="text-sm text-gray-400 ml-2"> ${product_price}</span>
+                <h2 className="font-bold text-2xl py-4 px-1">{product_title.slice(0,22)}</h2>
+                <div className="px-1">
+                    <span className=" text-gray-900 font-bold"> ${discount_price}</span>
+                    <span className="text-sm line-through font-bold text-gray-500 ml-2"> ${product_price}</span>
+                    <span className="text-red-500 text-xl font-bold px-3">30% OFF</span>
                 </div>
+                <p className="opacity-85 px-1 pt-5">{product_description.slice(0,70)}</p>
+                <button className="px-5 py-2.5 rounded-lg w-full text-white bg-black mt-5 ">Add to Cart</button>
 
             </div>
             
         </div>
     );
+};
+
+BlogContent.propTypes = {
+    product: PropTypes.object.isRequired,
 };
 
 export default BlogContent;
